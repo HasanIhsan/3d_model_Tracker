d7b1fbac9e9eb1c80c91591c6542086ec66ae3ca
Hassan Ihsan <ihsanhassan.business@gmail.com>
2025-03-10 07:59:18 -0700

    update: now able to set inital rotation

--- scene.py
+++ scene.py
@@ -16,8 +16,8 @@ class Scene:
         
         
         add(Cube(app))
-        add(Cube(app, tex_id=1, pos=(-2.5,0,0)))
-        add(Cube(app, tex_id=1, pos=(2.5, 0,0)))
+        add(Cube(app, tex_id=1, pos=(-2.5,0,0), rot=(45,0,0)))
+        add(Cube(app, tex_id=1, pos=(2.5, 0,0), rot=(0,0,45)))
         
     def render(self):
         for obj in self.objects:
